7bbbb9332affe2f35c9d156c47987446858244b2
Naomi Sharp <naomisharp@hotmail.co.uk>
2020-01-17 17:30:07 +0000

    tech and portfolio complete

--- app.js
+++ app.js
@@ -4,29 +4,32 @@ var app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+
+
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/home", function(req, res){
-    res.render("home");
+    res.render("home", {page: 'home'});
 });
 
 app.get("/about", function(req, res){
-    res.render("about");
+    res.render("about", {page: 'about'});
 });
 
 app.get("/portfolio", function(req, res){
-    res.render("portfolio");
+    res.render("portfolio", {page: 'portfolio'});
 });
 
 app.get("/tech", function(req, res){
-    res.render("tech");
+    res.render("tech", {page: 'tech'});
 });
 
 app.get("/contact", function(req, res){
-    res.render("contact");
+    res.render("contact", {page: 'contact'});
 });
+
 
 
 app.listen(process.env.PORT || 3000, process.env.IP, function(){
